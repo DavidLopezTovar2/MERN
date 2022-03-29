c396bbcdc17e5ac85c0d914eef450f29571136f0
DavidLopezTovar2 <47030693+DavidLopezTovar2@users.noreply.github.com>
2022-03-29 09:27:13 -0500

    Added project

---
+++ Pre-bootcamp/FirstProject/David_lopez_JS_Jugueteo.js
@@ -0,0 +1,13 @@
+
+
+
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(estatura){
+    if (estatura > 52){
+        console.log("¡Súbete, chico!");
+    } else {
+        console.log("Lo siento, chico. Tal vez el próximo año");
+    }
+}
+
+var alturaNiño = 32; 
+muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño);
