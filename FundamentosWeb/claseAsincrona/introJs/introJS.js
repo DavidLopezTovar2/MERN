f01b81f8e3d3d67068917b1b3d14444651d83f10
David López <dalopez@javeriana.edu.co>
2022-04-11 20:30:55 -0500

    HTML - JS
    Page working lo load from html and send form js

--- FundamentosWeb/claseAsincrona/introJs/introJS.js
+++ FundamentosWeb/claseAsincrona/introJs/introJS.js
@@ -1,4 +1,4 @@
-
+// PRIMERA CLASE ------------------------------------------------
 
 //var => variables, global type. Can't be modified
 var sal = 'hey';
@@ -31,7 +31,7 @@ const students = [student1,student2];
 
 //nadan oso solos
 //palabra palindromo ajeno
-let word = 'ajeno';
+let word = 'solos';
 
 function palindromo(word){
     for(let ini=0, end=word.length-1; ini<word.length/2; ini++, end--){        
@@ -42,10 +42,43 @@ function palindromo(word){
     return true;
 }
 
-let answer = palindromo(word);
 if (palindromo(word)){
     console.log(word + " = palindromo");
 }
 else{
     console.log(word + " = no es palindromo");
+}
+
+
+// SEGUNDA CLASE ------------------------------------------------
+
+function liqueurStore (age){
+    if (age < 18){
+        return 'No se puede vender';
+    }
+    else{
+        return 'Bienvenido';
+    }
+}
+
+function liqueurStoreTernary(age){
+    age < 18 ? 'No se puede vender':
+        'Bienvenido';
+}
+
+function greet(){
+    const username = document.getElementById('username').value;
+    //alert(`Hola ${username}`);
+    document.getElementById('message').innerHTML = username;
+}
+
+function onHover(){
+    console.log('Acabas de hacer hover');
+    let h1 = document.getElementsByTagName('h1')[0];
+    h1.style.backgroundColor = 'blue';
+}
+
+function hoverLeft(){
+    let h1 = document.getElementsByTagName('h1')[0];
+    h1.style.backgroundColor = 'white';
 }
