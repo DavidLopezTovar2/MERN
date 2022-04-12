edd9d564e9577f21708ca8016f533fe863dfcb40
David López <dalopez@javeriana.edu.co>
2022-04-11 19:11:49 -0500

    Palindromo

--- FundamentosWeb/claseAsincrona/introJs/introJS.js
+++ FundamentosWeb/claseAsincrona/introJs/introJS.js
@@ -2,7 +2,7 @@
 
 //var => variables, global type. Can't be modified
 var sal = 'hey';
-console.log(sal);
+//console.log(sal);
 
 
 //const y let => can change its value
@@ -27,16 +27,25 @@ const student2 = {
 
 const students = [student1,student2];
 
+//Palabra palindroma
 
+//nadan oso solos
+//palabra palindromo ajeno
+let word = 'ajeno';
 
-
-let word = ''
-palindromo(word);
-let palindromo = true;
 function palindromo(word){
-    for(let ini=0, end=word.length; ini<word/2; ini++, end--){
+    for(let ini=0, end=word.length-1; ini<word.length/2; ini++, end--){        
         if(word[ini] != word[end]){
             return false;
         }
     }
+    return true;
+}
+
+let answer = palindromo(word);
+if (palindromo(word)){
+    console.log(word + " = palindromo");
+}
+else{
+    console.log(word + " = no es palindromo");
 }
