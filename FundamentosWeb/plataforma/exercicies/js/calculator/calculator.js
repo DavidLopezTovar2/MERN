14b0560d48377b65d52d99ab795045d707e22c1d
David López <dalopez@javeriana.edu.co>
2022-04-29 01:15:19 -0500

    Calculator - first part
    Button completed

---
+++ FundamentosWeb/plataforma/exercicies/js/calculator/calculator.js
@@ -0,0 +1,10 @@
+
+function changeMainColor(typeButton, element) {
+    typeButton === 1 ? element.style.backgroundColor = '#808080':
+                        element.style.backgroundColor = '#09AE00';
+}
+
+function returnToMainColor(typeButton, element) {
+    typeButton === 1 ? element.style.backgroundColor = '#686868':
+                        element.style.backgroundColor = '#26F91B'
+}
