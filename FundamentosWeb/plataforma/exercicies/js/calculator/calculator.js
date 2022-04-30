da70e5d4ce0a484a558ded7c31b90490f5c979ae
David López <dalopez@javeriana.edu.co>
2022-04-29 23:19:19 -0500

    Calculator

--- FundamentosWeb/plataforma/exercicies/js/calculator/calculator.js
+++ FundamentosWeb/plataforma/exercicies/js/calculator/calculator.js
@@ -1,3 +1,10 @@
+var numberDisplay;
+var numberA
+var total;
+var operator;
+var completeNumber = '';
+var zeroNumber = '0';
+// Funtions for css styles
 
 function changeMainColor(typeButton, element) {
     typeButton === 1 ? element.style.backgroundColor = '#808080':
@@ -7,4 +14,45 @@ function changeMainColor(typeButton, element) {
 function returnToMainColor(typeButton, element) {
     typeButton === 1 ? element.style.backgroundColor = '#686868':
                         element.style.backgroundColor = '#26F91B'
+}
+
+// Functions for the calculator
+
+function press(number) {
+    numberDisplay = document.querySelector('#display');
+    number.toString();
+    completeNumber = completeNumber + number
+    numberDisplay.innerText = completeNumber;
+}
+
+function setOP(oper) {
+    total = parseFloat(completeNumber);
+    completeNumber = ''
+    numberA = total;
+    operator = oper;
+}
+
+function calculate(){
+    numberDisplay = document.querySelector('#display');
+    total = parseFloat(completeNumber);
+    console.log(numberA + '   ' + total);
+    console.log(operator);
+    if (operator === '+') {
+        total += numberA;
+    } else if (operator === '-'){
+        total = numberA - total;
+    }else if (operator === '/') {
+        total = numberA / total;
+    } else{
+        total *= numberA;
+    }
+    console.log('Total = ' + total);
+    operator = '';
+    numberDisplay.innerText = total;
+    completeNumber = total;
+}
+
+function clear() {
+    numberDisplay = document.querySelector('#display');
+    numberDisplay.innerText = zeroNumber;
 }
