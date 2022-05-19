35c44ec34ecf7e707c3e37e50446477ad8eeca29
David López <dalopez@javeriana.edu.co>
2022-05-19 11:39:00 -0400

    Convert to arrow

--- mern/asynchronous/fundamentals/classes.js
+++ mern/asynchronous/fundamentals/classes.js
@@ -33,3 +33,18 @@ class Special extends Sandwich {
 const fancySandwich = new Special('Serrano jam','queso','blanco','tomate');
 console.log(fancySandwich);
 
+
+
+
+console.log('Pregunta 3');
+console.log(banksClientsTaxNumbers());
+console.log('Pregunta 4');
+console.log(richClientsBalances());
+console.log('Pregunta 5');
+console.log(banksRankingByTotalBalance());
+console.log('Pregunta 6');
+console.log(banksFidelity());
+console.log('Pregunta 7');
+console.log(banksPoorClients());
+console.log('Pregunta 8');
+console.log(newClientRanking());
